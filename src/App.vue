2929baae8622e683b781ca8545f53381e92394d1
quan.phamanh <quan.phamanh@vti.com.vn>
2024-02-22 16:42:33 +0700

    divide component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
+  <the-header></the-header>
   <router-view />
+  <the-footer></the-footer>
 </template>
 
 <style lang="scss"></style>
+<script setup>
+  import TheHeader from "@/components/TheHeader.vue";
+  import TheFooter from "@/components/TheFooter.vue";
+</script>
---
+++ src/views/BlogView.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import TheTitleBar from "@/components/Blog/TheTitleBar.vue";
+import TheContent from "@/components/Blog/TheContent.vue";
+</script>
+
+<template>
+  <!-- Wrapper -->
+  <div id="wrapper">
+    <the-title-bar></the-title-bar>
+    <the-content></the-content>
+
+    <!-- Back To Top Button -->
+    <div id="backtotop"><a href="#"></a></div>
+  </div>
+  <!-- Wrapper / End -->
+</template>
+
+<style scoped lang="scss">
+
+</style>
